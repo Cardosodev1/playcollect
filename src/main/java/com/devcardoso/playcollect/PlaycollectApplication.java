99dc0d14ae857ed19f78c4736862fa35eb908391
Guilherme Cardoso <gcs200206@gmail.com>
2025-01-21 14:26:49 -0300

    Project created

---
+++ src/main/java/com/devcardoso/playcollect/PlaycollectApplication.java
@@ -0,0 +1,13 @@
+package com.devcardoso.playcollect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaycollectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaycollectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devcardoso/playcollect/PlaycollectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcardoso.playcollect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaycollectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
